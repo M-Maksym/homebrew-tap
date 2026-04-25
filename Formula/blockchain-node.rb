a3c2987d475ae86d85f4c70fdc55ebf686006901
Maks <m.meleshko@cargoo.io>
2026-04-25 10:07:22 +0300

    blockchain-node v1.1.1

--- Formula/blockchain-node.rb
+++ Formula/blockchain-node.rb
@@ -2,13 +2,13 @@
 class BlockchainNode < Formula
   desc "Proof-of-Stake blockchain node with terminal UI"
   homepage "https://github.com/M-Maksym/blockchain-pos"
-  version "1.1.0"
+  version "1.1.1"
 
   depends_on "node"
   depends_on :macos
 
   url "https://github.com/M-Maksym/blockchain-pos-releases/releases/download/v#{version}/blockchain-node-macos-v#{version}.tar.gz"
-  sha256 "4e928b5a0255aed4aca49dc2433e2c0d5a3d4826c673ed1cb54e709ad6079882"
+  sha256 "df5c84f8114979ed34605f7510f53d886828bba8c1f91d7c26d7804f96ddfc15"
 
   def install
     libexec.install "app.mjs"
